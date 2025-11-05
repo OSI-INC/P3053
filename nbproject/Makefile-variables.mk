ad577fd0a40e4f52b5e57842dc3c07a8fa9c7fc3
Kevan Hashemi <hashemi@opensourceinstruments.com>
2025-11-05 11:13:51 -0500

    No more nbproject, two makefiles.

--- nbproject/Makefile-variables.mk
+++
@@ -1,6 +0,0 @@
-# This file began as an auto-generated MPLAB in the Harmony3 repository of example
-# applications. 
-CND_BASEDIR=`pwd`
-CND_ARTIFACT_DIR_pic32mz_ef_sk=dist/pic32mz_ef_sk/production
-CND_ARTIFACT_NAME_pic32mz_ef_sk=P3053.production.hex
-CND_ARTIFACT_PATH_pic32mz_ef_sk=dist/pic32mz_ef_sk/production/P3053.production.hex
