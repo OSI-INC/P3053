3f9f5eae62b7de51a47cad7033863fdbe568152a
Kevan Hashemi <hashemi@opensourceinstruments.com>
2025-11-05 08:28:15 -0500

    Adjusting makefile comments, testing gitignore.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -1,10 +1,6 @@
-#
-# Generated - do not edit!
-#
-# NOCDDL
-#
+# This file began as an auto-generated MPLAB in the Harmony3 repository of example
+# applications. 
 CND_BASEDIR=`pwd`
-# pic32mz_ef_sk configuration
 CND_ARTIFACT_DIR_pic32mz_ef_sk=dist/pic32mz_ef_sk/production
 CND_ARTIFACT_NAME_pic32mz_ef_sk=P3053.production.hex
 CND_ARTIFACT_PATH_pic32mz_ef_sk=dist/pic32mz_ef_sk/production/P3053.production.hex
