294c51a2ab96685a8ac05004ef5439198cc0e0cd
Kevan Hashemi <hashemi@opensourceinstruments.com>
2025-11-04 22:17:23 -0500

    Build independent of MPLAB.

---
+++ nbproject/Makefile-local-pic32mz_ef_sk.mk
@@ -0,0 +1,30 @@
+#
+# Generated Makefile - do not edit!
+#
+#
+# This file contains information about the location of compilers and other tools.
+# If you commmit this file into your revision control server, you will be able to 
+# to checkout the project and build it from the command line with make. However,
+# if more than one person works on the same project, then this file might show
+# conflicts since different users are bound to have compilers in different places.
+# In that case you might choose to not commit this file and let MPLAB X recreate this file
+# for each user. The disadvantage of not commiting this file is that you must run MPLAB X at
+# least once so the file gets created and the project can be built. Finally, you can also
+# avoid using this file at all if you are only building from the command line with make.
+# You can invoke make with the values of the macros:
+# $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
+#
+OS_CURRENT="$(shell uname -s)"
+MP_CC="/Applications/microchip/xc32/v4.60/bin/xc32-gcc"
+MP_CPPC="/Applications/microchip/xc32/v4.60/bin/xc32-g++"
+# MP_BC is not defined
+MP_AS="/Applications/microchip/xc32/v4.60/bin/xc32-as"
+MP_LD="/Applications/microchip/xc32/v4.60/bin/xc32-ld"
+MP_AR="/Applications/microchip/xc32/v4.60/bin/xc32-ar"
+MP_CC_DIR="/Applications/microchip/xc32/v4.60/bin"
+MP_CPPC_DIR="/Applications/microchip/xc32/v4.60/bin"
+# MP_BC_DIR is not defined
+MP_AS_DIR="/Applications/microchip/xc32/v4.60/bin"
+MP_LD_DIR="/Applications/microchip/xc32/v4.60/bin"
+MP_AR_DIR="/Applications/microchip/xc32/v4.60/bin"
+DFP_DIR=/Users/kevan/Code/Harmony3/dev_packs/Microchip/PIC32MZ-EF_DFP/1.3.58
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mz_ef_sk configuration
+CND_ARTIFACT_DIR_pic32mz_ef_sk=dist/pic32mz_ef_sk/production
+CND_ARTIFACT_NAME_pic32mz_ef_sk=P3053.production.hex
+CND_ARTIFACT_PATH_pic32mz_ef_sk=dist/pic32mz_ef_sk/production/P3053.production.hex
