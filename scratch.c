071f97ec5195afe7ff0384da573e2fd75895d0c7
Kevan Hashemi <hashemi@opensourceinstruments.com>
2025-12-03 18:52:59 -0500

    Tidy up scratch.c

--- scratch.c
+++ scratch.c
@@ -1,39 +1,14 @@
-	n = snprintf(&out[i],max_len-i,"Interface : %s\r\n",
-		name);
-	i = i + n;
-
 /*
-	Routines to set up our parallel bus.
+	Routines to set up our parallel bus. Suppose we re-wire the A3053A so that
+	CA0-CA7 are on RA0-RA7, /CW on RA9,
+	/CDS on RA10, and we have CD0-CD7 on RE0-RE7. Here is how ChatGPT says we
+	can configure these ports.
 */
 
 #include <xc.h>
 
-// This is where we want to be as fast as possible. With the above complex 
-// construction, the stream read will take about 50 clock cycles, or 250 ns,
-// plus however long we have to wait for the controller to respond, which 
-// will be around 50 ns.
-uint8_t bus_read_same_address(void) {
-    ds_assert();
-    // wait states
-    uint8_t v = cd_read_bus();
-    ds_deassert();
-    return v;
-}
-
-
-
-/*
-	Suppose we re-wire the A3053A so that CA0-CA7 are on RA0-RA7, /CW on RA9,
-	/CDS on RA10, and we have CD0-CD7 on RE0-RE7. Here is how ChatGPT says we
-	can configure these ports.
-*/
-// Address bus: RA0–RA7
 #define CA_MASK_A      0x00FFu     // bits 0..7 on PORTA
-
-// Data bus: RE0–RE7
 #define CD_MASK_E      0x00FFu     // bits 0..7 on PORTE
-
-// Control lines on PORTA
 #define CW_BIT_A       (1u << 9)   // RA9  = /CW (active low)
 #define CDS_BIT_A      (1u << 10)  // RA10 = /CDS (active low)
 
@@ -60,17 +35,14 @@ ANSELxCLR makes the pins digital.
 */
 
 static inline void cd_bus_as_outputs(void) {
-    // RE0–7 as outputs
     TRISECLR = CD_MASK_E;
 }
 
 static inline void cd_bus_as_inputs(void) {
-    // RE0–7 as inputs (tri-stated from your side)
     TRISESET = CD_MASK_E;
 }
 
 static inline void bus_set_address(uint8_t addr) {
-    // Clear lower 8 bits then OR in the new address
     LATA = (LATA & ~CA_MASK_A) | ((uint32_t)addr & 0xFFu);
 }
 
@@ -79,73 +51,77 @@ static inline void cw_deassert(void) { LATASET = CW_BIT_A; }  // /CW high
 static inline void cds_assert(void)   { LATACLR = CDS_BIT_A; } // /CDS low
 static inline void cds_deassert(void) { LATASET = CDS_BIT_A; } // /CDS high
 
-// Full bus write byte cycle including setting address, /CDW, and driving data lines.
 void bus_write_byte(uint8_t addr, uint8_t data) {
     bus_set_address(addr);
     cw_assert();
     cd_bus_as_outputs();
     LATE = (LATE & ~CD_MASK_E) | ((uint32_t)data & 0xFFu);
     cds_assert();
-    // wait states
+    cds_assert();
+    cds_assert();
+    cds_assert();
+    cds_assert();
     cds_deassert();
 }
 
-// If we want to write again to the same address, but a different value as in stream
-// write, we can do:
 void cd_write_bus(uint8_t data) {
     LATE = (LATE & ~CD_MASK_E) | ((uint32_t)data & 0xFFu);
     cds_assert();
-    // wait states
+    cds_assert();
+    cds_assert();
+    cds_assert();
+    cds_assert();
     cds_deassert();
 }
 
-// Here is a complete read cycle.
 uint8_t bus_read_byte(uint8_t addr) {
     uint8_t value;
     bus_set_address(addr);
     cd_bus_as_inputs();
     cw_deassert(); 
     cds_assert();
-    // wait states
+    cds_assert();
+    cds_assert();
+    cds_assert();
+    cds_assert();
     value = (uint8_t)(PORTE & CD_MASK_E);
     cds_deassert();
     return value;
 }
 
-// The stream read takes about 10 clock cycles with the above, simpler register
-// allocations.
 uint8_t cd_read_bus(void) {
     cds_assert();
-    // wait states
+    cds_assert();
+    cds_assert();
+    cds_assert();
+    cds_assert();
     value = (uint8_t)(PORTE & CD_MASK_E);
     cds_deassert();
     return value;
 }
 
-// Harmony Code Warning:
-// ---------------------
-//
-// Below you will find the original TCP/IP echo server from the Harmony example
-// project. It contains a serious bug. Its management of the receive buffer
-// violates the range of the receive buffer. Because the receive buffer resides
-// in the stack, writing past the end of the buffer overwrites a location in the
-// stack. With the receive buffer declaration where it is in the code, it just
-// so happens that the the location above the buffer in the stack is not vital
-// to the operation of the program. But if we move the buffer declaration to the
-// top of the routine, the compiler places the buffer below a vital location,
-// the stack gets trashed by APP_Tasks, and the entire program crashes. So far
-// as we can tell, the authors placed the buffer declaration where it is now
-// during development. They probably tried to move the buffer declaration to the
-// top of the procedure when they had the code working, found that the code
-// crashed, could not figure out why in the time they had available, and decided
-// to restore the declaration to its development location and move on to their
-// next programming project.
-//
-// Our warning is this: assume that all of the Harmony code is written in the
-// same spirit. The code works within the bounds that it was tested, but any
-// change that you make to it, however trivial, even in the ordering or location
-// of variable declarations, can break the code.
-//
+/*
+App_Tasks, shown below, is the original TCP/IP echo server from the Harmony
+example project. It contains a serious bug. Its management of the receive buffer
+violates the range of the receive buffer. Because the receive buffer resides in
+the stack, writing past the end of the buffer overwrites a location in the
+stack. With the receive buffer declaration where it is in the code, it just so
+happens that the the location above the buffer in the stack is not vital to the
+operation of the program. But if we move the buffer declaration to the top of
+the routine, the compiler places the buffer below a vital location, the stack
+gets trashed by APP_Tasks, and the entire program crashes. So far as we can
+tell, the authors placed the buffer declaration where it is now during
+development. They probably tried to move the buffer declaration to the top of
+the procedure when they had the code working, found that the code crashed, could
+not figure out why in the time they had available, and decided to restore the
+declaration to its development location and move on to their next programming
+project.
+
+WARNING: assume that all of the Harmony code is written in the same spirit. The
+code works within the bounds that it was tested, but any change that you make to
+it, however trivial, even in the ordering or location of variable declarations,
+can break the code.
+*/
 void APP_Tasks ( void )
 {
     SYS_STATUS          tcpipStat;
